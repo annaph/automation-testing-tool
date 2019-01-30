e5ba3b5bc8fd6fff340b5098372b5e83d0f79916
annaph <anna.philips@mail.com>
2019-01-30 22:39:05 +0000

    Model-work in progress

--- src/main/scala/org/cartagena/tool/core/Process.scala
+++ src/main/scala/org/cartagena/tool/core/Process.scala
@@ -2,26 +2,37 @@ package org.cartagena.tool.core
 
 sealed trait Process[I, O] {
 
-  def apply(s: Stream[O]): Stream[O] = this match {
+  def apply(s: Stream[I]): Stream[O] = this match {
     case Await(recv) => s match {
-      case h #:: t => ???
-      case xs => ???
+      case h #:: t =>
+        recv(Some(h))(t)
+      case xs =>
+        recv(None)(xs)
     }
     case Emit(h, t) =>
       h #:: t(s)
-    case Halt(End) =>
+    case Halt() =>
       Stream()
-    case Halt(e) =>
-      throw new Exception("Error running process!", e)
+  }
+
+  def repeat: Process[I, O] = {
+    def go(p: Process[I, O]): Process[I, O] = p match {
+      case Halt() => go(this)
+      case Await(recv) => ???
+      case Emit(h, t) =>
+        Emit(h, go(t))
+    }
+
+    go(this)
   }
 
 }
 
 case class Await[I, O](recv: Option[I] => Process[I, O]) extends Process[I, O]
 
-case class Emit[I, O](head: O, tail: Process[I,O] = Halt(End)) extends Process[I, O]
+case class Emit[I, O](head: O, tail: Process[I,O] = Halt[I, O]()) extends Process[I, O]
 
-case class Halt[I, O](err: Throwable = End) extends Process[I, O]
+case class Halt[I, O]() extends Process[I, O]
 
 case object End extends Exception
 
@@ -29,10 +40,11 @@ case object Kill extends Exception
 
 object Process {
 
-}
-
-class TestCaseProcess extends Process[Step, Step]
-
-object TestCaseProcess {
+  def liftOne[I, O](f: I => O): Process[I, O] = Await{
+    case Some(i) =>
+      Emit(f(i))
+    case None =>
+      Halt()
+  }
 
 }
