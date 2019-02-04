43c14fee038cc7998ca45aedb0c6ea64ed06653d
annaph <anna.philips@mail.com>
2019-02-04 19:21:35 +0000

    Model - work in progress

--- src/main/scala/org/cartagena/tool/core/Process.scala
+++ src/main/scala/org/cartagena/tool/core/Process.scala
@@ -12,25 +12,40 @@ sealed trait Process[I, O] {
     case Emit(h, t) =>
       h #:: t(s)
     case Halt() =>
-      Stream()
+      Stream.empty
   }
 
   def repeat: Process[I, O] = {
     def go(p: Process[I, O]): Process[I, O] = p match {
-      case Halt() => go(this)
-      case Await(recv) => ???
+      case Await(recv) => Await {
+        case None =>
+          recv(None)
+        case i =>
+          go(recv(i))
+      }
       case Emit(h, t) =>
         Emit(h, go(t))
+      case Halt() =>
+        go(this)
     }
 
     go(this)
   }
 
+  def |>[O2](p2: Process[O, O2]): Process[I, O2] =
+    pipe(p2)
+
+  def pipe[O2](p2: Process[O, O2]): Process[I, O2] =
+    Process.pipe(this, p2)
+
+  def map[O2](f: O => O2): Process[I, O2] =
+    this |> Process.lift(f)
+
 }
 
 case class Await[I, O](recv: Option[I] => Process[I, O]) extends Process[I, O]
 
-case class Emit[I, O](head: O, tail: Process[I,O] = Halt[I, O]()) extends Process[I, O]
+case class Emit[I, O](head: O, tail: Process[I, O] = Halt[I, O]()) extends Process[I, O]
 
 case class Halt[I, O]() extends Process[I, O]
 
@@ -40,11 +55,101 @@ case object Kill extends Exception
 
 object Process {
 
-  def liftOne[I, O](f: I => O): Process[I, O] = Await{
+  def pipe[I, O, O2](p1: Process[I, O], p2: Process[O, O2]): Process[I, O2] = {
+    def go(pr1: Process[I, O], pr2: Process[O, O2]): Process[I, O2] = pr1 match {
+      case Await(recv1) => pr2 match {
+        case Await(_) => await[I, O2] {
+          case Some(i) =>
+            go(recv1(Some(i)), pr2)
+          case _ =>
+            halt
+        }
+        case Emit(h2, t2) =>
+          emit(h2, go(pr1, t2))
+        case Halt() =>
+          halt
+      }
+
+      case Emit(h1, t1) => pr2 match {
+        case Await(recv2) =>
+          go(t1, recv2(Some(h1)))
+        case Emit(h2, t2) =>
+          emit(h2, go(t1, t2))
+        case Halt() =>
+          halt
+      }
+
+      case Halt() =>
+        halt
+    }
+
+    go(p1, p2)
+  }
+
+  def lift[I, O](f: I => O): Process[I, O] = await[I, O] {
     case Some(i) =>
-      Emit(f(i))
+      emit(f(i))
     case None =>
-      Halt()
+      halt
+  }.repeat
+
+  def await[I, O](recv: Option[I] => Process[I, O]): Process[I, O] =
+    Await(recv)
+
+  def emit[I, O](head: O, tail: Process[I, O] = Halt[I, O]()): Process[I, O] =
+    Emit(head, tail)
+
+  def halt[I, O]: Process[I, O] =
+    Halt()
+
+  def filter[I](p: I => Boolean): Process[I, I] = await[I, I] {
+    case Some(i) if p(i) =>
+      emit(i)
+    case _ =>
+      halt
+  }.repeat
+
+  def take[I](n: Int): Process[I, I] = await[I, I] {
+    case Some(i) if n > 0 =>
+      emit(i, take(n - 1))
+    case _ =>
+      halt
+  }
+
+  def drop[I](n: Int): Process[I, I] = await[I, I] {
+    case Some(i) if n > 0 =>
+      drop(n - 1)
+    case Some(i) if n == 0 =>
+      emit(i, drop(0))
+    case _ =>
+      halt
+  }
+
+  def takeWhile[I](f: I => Boolean): Process[I, I] = await[I, I] {
+    case Some(i) if f(i) =>
+      Emit(i, takeWhile(f))
+    case _ =>
+      halt
+  }
+
+  def dropWhile[I](f: I => Boolean): Process[I, I] = await[I, I] {
+    case Some(i) if f(i) =>
+      dropWhile(f)
+    case Some(i) =>
+      emit(i, dropWhile(f))
+    case _ =>
+      halt
+  }
+
+  def count[I]: Process[I, Int] = {
+    def go(c: Int): Process[I, Int] = await[I, Int] {
+      case Some(i) =>
+        emit(c, go(c + 1))
+      case _ =>
+        halt
+    }
+
+    go(1)
   }
 
 }
--- src/main/scala/org/cartagena/tool/suite/login/LoginFunctionality.scala
+++ src/main/scala/org/cartagena/tool/suite/login/LoginFunctionality.scala
@@ -1,6 +1,7 @@
 package org.cartagena.tool.suite.login
 
 import org.cartagena.tool.core._
+import org.cartagena.tool.core.Process._
 
 object LoginFunctionalityAndLocalHostProfile extends App {
 
@@ -11,5 +12,50 @@ object LoginFunctionalityAndLocalHostProfile extends App {
 
   val steps = suite.getSteps.map(_.name).mkString("\n")
   println("\n" + steps)
+
+  val f: Step => Step = step => {
+    println("-------------------------")
+    println(step.name)
+    step.execute()
+    println("-------------------------")
+
+    step
+  }
+
+  println("Lift example:")
+  val l = lift(f)(suite.getSteps).toList.map(_.name)
+  println(l mkString "; ")
+
+  println("Filter example:")
+  val ints = filter[Int](x => x % 2 == 0)(Stream(1, 2, 3, 4))
+  println(ints mkString ";")
+
+  println("Take example:")
+  val ints2 = take(3)(Stream(1, 2, 3, 4, 5))
+  println(ints2 mkString ";")
+
+  println("Drop example:")
+  val ints3 = drop(2)(Stream(1, 2, 3, 4, 5))
+  println(ints3 mkString ";")
+
+  println("Take While example:")
+  val ints4 = takeWhile[Int](_ < 4)(Stream(1, 2, 3, 4, 5))
+  println(ints4 mkString ";")
+
+  println("Drop While example:")
+  val ints5 = dropWhile[Int](_ < 3)(Stream(1, 2, 3, 4, 5))
+  println(ints5 mkString ";")
+
+  println("Count example:")
+  val chars = count(Stream("a", "b", "c"))
+  println(chars mkString ";")
+
+  println("Pipe example:")
+  val ints6 = filter[Int](x => x % 2 == 0) |> lift(_ + 1)
+  println(ints6(Stream(1, 2, 3, 4, 5, 6)) mkString ";")
+
+  println("Pipe example 2:")
+  val ints7: Process[Int, Int] = take(3) |> lift(_ * 3)
+  println(ints7(Stream(1, 2, 3, 4, 5, 6)) mkString ";")
 
 }
