5e00274afeae6915dfe90952e97404f63695f887
annaph <anna.philips@mail.com>
2019-02-06 20:57:07 +0000

    Model - work in progress

--- src/main/scala/org/cartagena/tool/core/Process.scala
+++ src/main/scala/org/cartagena/tool/core/Process.scala
@@ -1,36 +1,26 @@
 package org.cartagena.tool.core
 
+import scala.util.{Failure, Success, Try}
+
 sealed trait Process[I, O] {
 
-  def apply(s: Stream[I]): Stream[O] = this match {
-    case Await(recv) => s match {
-      case h #:: t =>
-        recv(Some(h))(t)
-      case xs =>
-        recv(None)(xs)
-    }
-    case Emit(h, t) =>
-      h #:: t(s)
-    case Halt() =>
-      Stream.empty
-  }
+  def apply(s: Stream[I]): Stream[Try[O]] =
+    Process.apply(this, s)
 
-  def repeat: Process[I, O] = {
-    def go(p: Process[I, O]): Process[I, O] = p match {
-      case Await(recv) => Await {
-        case None =>
-          recv(None)
-        case i =>
-          go(recv(i))
-      }
-      case Emit(h, t) =>
-        Emit(h, go(t))
-      case Halt() =>
-        go(this)
-    }
+  def unit(o: O): Process[I, O] =
+    Process.emit(o)
 
-    go(this)
-  }
+  def map[O2](f: O => O2): Process[I, O2] =
+    Process.map(this)(f)
+
+  def flatMap[O2](f: O => Process[I, O2]): Process[I, O2] =
+    Process.flatMap(this)(f)
+
+  def repeat: Process[I, O] =
+    Process.repeat(this)
+
+  def onHalt(f: Throwable => Process[I, O]): Process[I, O] =
+    Process.onHalt(this)(f)
 
   def |>[O2](p2: Process[O, O2]): Process[I, O2] =
     pipe(p2)
@@ -38,22 +28,101 @@ sealed trait Process[I, O] {
   def pipe[O2](p2: Process[O, O2]): Process[I, O2] =
     Process.pipe(this, p2)
 
-  def map[O2](f: O => O2): Process[I, O2] =
-    this |> Process.lift(f)
+  def append(p: Process[I, O]): Process[I, O] =
+    Process.append(this, p)
+
+  def ++(p: Process[I, O]): Process[I, O] =
+    append(p)
 
 }
 
 case class Await[I, O](recv: Option[I] => Process[I, O]) extends Process[I, O]
 
-case class Emit[I, O](head: O, tail: Process[I, O] = Halt[I, O]()) extends Process[I, O]
+case class Emit[I, O](head: O, tail: Process[I, O] = Halt[I, O](End)) extends Process[I, O]
 
-case class Halt[I, O]() extends Process[I, O]
+case class Halt[I, O](err: Throwable) extends Process[I, O]
 
 case object End extends Exception
 
 case object Kill extends Exception
 
 object Process {
+
+  def apply[I, O](p: Process[I, O], s: Stream[I]): Stream[Try[O]] = p match {
+    case Await(recv) => s match {
+      case h #:: t =>
+        `try`(recv(Some(h)))(t)
+      case xs =>
+        `try`(recv(None))(xs)
+    }
+    case Emit(h, t) =>
+      Success(h) #:: t(s)
+    case Halt(End | Kill) =>
+      Stream.empty
+    case Halt(err) =>
+      Failure(err) #:: Stream.empty[Try[O]]
+  }
+
+  def await[I, O](recv: Option[I] => Process[I, O]): Process[I, O] =
+    Await(recv)
+
+  def emit[I, O](head: O, tail: Process[I, O] = Halt[I, O](End)): Process[I, O] =
+    Emit(head, tail)
+
+  def halt[I, O]: Process[I, O] =
+    Halt(End)
+
+  def halt[I, O](err: Throwable): Process[I, O] =
+    Halt(err)
+
+  def unit[I, O](o: O): Process[I, O] =
+    emit(o)
+
+  def map[I, O, O2](p: Process[I, O])(f: O => O2): Process[I, O2] =
+    p |> lift(f)
+
+  def flatMap[I, O, O2](p: Process[I, O])(f: O => Process[I, O2]): Process[I, O2] = p match {
+    case Await(recv) =>
+      await(recv andThen (_ flatMap f))
+    case Emit(h, t) =>
+      `try`(f(h)) ++ t.flatMap(f)
+    case Halt(err) =>
+      halt(err)
+  }
+
+  def `try`[I, O](p: => Process[I, O]): Process[I, O] =
+    Try(p) match {
+      case Success(_) =>
+        p
+      case Failure(err) =>
+        halt(err)
+    }
+
+  def repeat[I, O](p: Process[I, O]): Process[I, O] = {
+    def go(pr: Process[I, O]): Process[I, O] = pr match {
+      case Await(recv) => await {
+        case None =>
+          recv(None)
+        case i =>
+          go(recv(i))
+      }
+      case Emit(h, t) =>
+        emit(h, go(t))
+      case Halt(_) =>
+        go(p)
+    }
+
+    go(p)
+  }
+
+  def onHalt[I, O](p: Process[I, O])(f: Throwable => Process[I, O]): Process[I, O] = p match {
+    case Await(recv) =>
+      await(recv andThen (_ onHalt f))
+    case Emit(h, t) =>
+      emit(h, t onHalt f)
+    case Halt(e) =>
+      `try`(f(e))
+  }
 
   def pipe[I, O, O2](p1: Process[I, O], p2: Process[O, O2]): Process[I, O2] = {
     def go(pr1: Process[I, O], pr2: Process[O, O2]): Process[I, O2] = pr1 match {
@@ -66,41 +135,41 @@ object Process {
         }
         case Emit(h2, t2) =>
           emit(h2, go(pr1, t2))
-        case Halt() =>
+        case Halt(_) =>
           halt
       }
-
       case Emit(h1, t1) => pr2 match {
         case Await(recv2) =>
           go(t1, recv2(Some(h1)))
         case Emit(h2, t2) =>
           emit(h2, go(t1, t2))
-        case Halt() =>
+        case Halt(_) =>
           halt
       }
-
-      case Halt() =>
+      case Halt(_) =>
         halt
     }
 
     go(p1, p2)
   }
 
-  def lift[I, O](f: I => O): Process[I, O] = await[I, O] {
+  def append[I, O](p1: Process[I, O], p2: Process[I, O]): Process[I, O] =
+    p1.onHalt {
+      case End =>
+        p2
+      case err =>
+        halt(err)
+    }
+
+  def liftOne[I, O](f: I => O): Process[I, O] = await[I, O] {
     case Some(i) =>
       emit(f(i))
     case None =>
       halt
-  }.repeat
+  }
 
-  def await[I, O](recv: Option[I] => Process[I, O]): Process[I, O] =
-    Await(recv)
-
-  def emit[I, O](head: O, tail: Process[I, O] = Halt[I, O]()): Process[I, O] =
-    Emit(head, tail)
-
-  def halt[I, O]: Process[I, O] =
-    Halt()
+  def lift[I, O](f: I => O): Process[I, O] =
+    liftOne(f).repeat
 
   def filter[I](p: I => Boolean): Process[I, I] = await[I, I] {
     case Some(i) if p(i) =>
@@ -110,8 +179,10 @@ object Process {
   }.repeat
 
   def take[I](n: Int): Process[I, I] = await[I, I] {
-    case Some(i) if n > 0 =>
+    case Some(i) if n > 1 =>
       emit(i, take(n - 1))
+    case Some(i) if n == 1 =>
+      emit(i)
     case _ =>
       halt
   }
@@ -143,13 +214,35 @@ object Process {
 
   def count[I]: Process[I, Int] = {
     def go(c: Int): Process[I, Int] = await[I, Int] {
-      case Some(i) =>
+      case Some(_) =>
         emit(c, go(c + 1))
       case _ =>
         halt
     }
 
     go(1)
+  }
+
+  def zipWithIndex[I]: Process[I, (I, Int)] = {
+    def go(c: Int): Process[I, (I, Int)] = await[I, (I, Int)] {
+      case Some(i) =>
+        emit(c).flatMap { index =>
+          emit[I, (I, Int)](i -> index, go(c + 1))
+        }
+      case _ =>
+        halt
+    }
+
+    go(0)
+  }
+
+  def exists[I](f: I => Boolean): Process[I, Boolean] = await[I, Boolean] {
+    case Some(i) if !f(i) =>
+      emit(false, exists(f))
+    case Some(i) if f(i) =>
+      emit(true)
+    case _ =>
+      halt
   }
 
 }
--- src/main/scala/org/cartagena/tool/suite/login/LoginFunctionality.scala
+++ src/main/scala/org/cartagena/tool/suite/login/LoginFunctionality.scala
@@ -22,40 +22,152 @@ object LoginFunctionalityAndLocalHostProfile extends App {
     step
   }
 
+  println("------------------------------------------------------------------------------------------------------")
   println("Lift example:")
-  val l = lift(f)(suite.getSteps).toList.map(_.name)
+  val l = lift(f)(suite.getSteps).toList.map(_.get.name)
   println(l mkString "; ")
+  println("------------------------------------------------------------------------------------------------------")
 
+  println("------------------------------------------------------------------------------------------------------")
   println("Filter example:")
   val ints = filter[Int](x => x % 2 == 0)(Stream(1, 2, 3, 4))
-  println(ints mkString ";")
+  println(ints.map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (2, 4)")
+  println("------------------------------------------------------------------------------------------------------")
 
+  println("------------------------------------------------------------------------------------------------------")
   println("Take example:")
   val ints2 = take(3)(Stream(1, 2, 3, 4, 5))
-  println(ints2 mkString ";")
+  println(ints2.map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (1, 2, 3)")
+  println("------------------------------------------------------------------------------------------------------")
 
+  println("------------------------------------------------------------------------------------------------------")
   println("Drop example:")
   val ints3 = drop(2)(Stream(1, 2, 3, 4, 5))
-  println(ints3 mkString ";")
+  println(ints3.map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (3, 4, 5)")
+  println("------------------------------------------------------------------------------------------------------")
 
+  println("------------------------------------------------------------------------------------------------------")
   println("Take While example:")
   val ints4 = takeWhile[Int](_ < 4)(Stream(1, 2, 3, 4, 5))
-  println(ints4 mkString ";")
+  println(ints4.map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (1, 2, ,3)")
+  println("------------------------------------------------------------------------------------------------------")
 
+  println("------------------------------------------------------------------------------------------------------")
   println("Drop While example:")
   val ints5 = dropWhile[Int](_ < 3)(Stream(1, 2, 3, 4, 5))
-  println(ints5 mkString ";")
+  println(ints5.map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (3, 4, 5)")
+  println("------------------------------------------------------------------------------------------------------")
 
+  println("------------------------------------------------------------------------------------------------------")
   println("Count example:")
   val chars = count(Stream("a", "b", "c"))
-  println(chars mkString ";")
+  println(chars.map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (1, 2, 3)")
+  println("------------------------------------------------------------------------------------------------------")
 
+  println("------------------------------------------------------------------------------------------------------")
   println("Pipe example:")
   val ints6 = filter[Int](x => x % 2 == 0) |> lift(_ + 1)
-  println(ints6(Stream(1, 2, 3, 4, 5, 6)) mkString ";")
+  println(ints6(Stream(1, 2, 3, 4, 5, 6)).map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (3, 5, 7)")
+  println("------------------------------------------------------------------------------------------------------")
 
+  println("------------------------------------------------------------------------------------------------------")
   println("Pipe example 2:")
   val ints7: Process[Int, Int] = take(3) |> lift(_ * 3)
-  println(ints7(Stream(1, 2, 3, 4, 5, 6)) mkString ";")
+  println(ints7(Stream(1, 2, 3, 4, 5, 6)).map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (3, 6, 9)")
+  println("------------------------------------------------------------------------------------------------------")
+
+  println("------------------------------------------------------------------------------------------------------")
+  println("Append example:")
+  val ints8: Process[Int, Int] = take(3) ++ lift(_ + 3)
+  print(ints8(Stream(1, 2, 3, 4, 5, 6)).map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (1, 2, 3, 7, 8, 9)")
+  println("------------------------------------------------------------------------------------------------------")
+
+  println("------------------------------------------------------------------------------------------------------")
+  println("Zip With Index example:")
+  val ints9: Process[String, (String, Int)] = zipWithIndex
+  print(ints9(Stream("A", "B", "C")).map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (A0, B1, C2)")
+  println("------------------------------------------------------------------------------------------------------")
+
+  println("------------------------------------------------------------------------------------------------------")
+  println("Exists example:")
+  val ints10: Process[Int, Boolean] = exists(_ % 2 == 0)
+  print(ints10(Stream(1, 3, 5, 6, 7)).map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (F, F, F, T)")
+  println("------------------------------------------------------------------------------------------------------")
+
+  println("------------------------------------------------------------------------------------------------------")
+  println("Exists example 2:")
+  val ints11: Process[Int, Boolean] = count |> exists(_ > 2)
+  print(ints11(Stream(1, 3, 5, 6, 7)).map(_.get) mkString ";")
+  println("\t\t\t ===> Should be: (F, F, T)")
+  println("------------------------------------------------------------------------------------------------------")
+
+  println("------------------------------------------------------------------------------------------------------")
+  println("Halt(err) example:")
+  val ints12: Process[String, Int] = lift(_.toInt)
+  print(ints12(Stream("1", "2", "A")) mkString ";")
+  println("\t\t\t ===> Should be: (S(1), S(2), F(err))")
+  println("------------------------------------------------------------------------------------------------------")
+
+  println("------------------------------------------------------------------------------------------------------")
+  println("Halt(Kill) example:")
+  val ints13: Process[Int, Int] = lift {
+    case 3 => throw Kill
+    case x => x
+  }
+  print(ints13(Stream(1, 2, 3, 4, 5)) mkString ";")
+  println("\t\t\t ===> Should be: (S(1), S(2))")
+  println("------------------------------------------------------------------------------------------------------")
+
+  println("------------------------------------------------------------------------------------------------------")
+  println("Append & Halt(err) example:")
+  val ints14: Process[Int, Int] = lift[Int, Int] {
+    case 3 => throw new Exception
+    case x => x
+  } ++ lift[Int, Int](_ + 3)
+  print(ints14(Stream(1, 2, 3, 4, 5, 6)) mkString ";")
+  println("\t\t\t ===> Should be: (S(1), S(2), F(err)) ")
+  println("------------------------------------------------------------------------------------------------------")
+
+  println("------------------------------------------------------------------------------------------------------")
+  println("Append & Halt(Kill) example:")
+  val ints15: Process[Int, Int] = lift[Int, Int] {
+    case 3 => throw Kill
+    case x => x
+  } ++ lift[Int, Int](_ + 3)
+  print(ints15(Stream(1, 2, 3, 4, 5, 6)) mkString ";")
+  println("\t\t\t ===> Should be: (S(1), S(2))")
+  println("------------------------------------------------------------------------------------------------------")
+
+  println("------------------------------------------------------------------------------------------------------")
+  println("Flat Map + Halt(err) example:")
+  val ints16: Process[Int, Int] = lift[Int, Int] {
+    case 3 => throw new Exception
+    case x => x
+  }.flatMap(n => Emit(n))
+  print(ints16(Stream(1, 2, 3, 4, 5, 6)) mkString ";")
+  println("\t\t\t ===> Should be: (S(1), S(2), F(err))")
+  println("------------------------------------------------------------------------------------------------------")
+
+  println("------------------------------------------------------------------------------------------------------")
+  println("Flat Map + Halt(Kill) example:")
+  val ints17: Process[Int, Int] = lift[Int, Int] {
+    case 3 => throw Kill
+    case x => x
+  }.flatMap(n => Emit(n))
+  print(ints16(Stream(1, 2, 3, 4, 5, 6)) mkString ";")
+  println("\t\t\t ===> Should be: (S(1), S(2))")
+  println("------------------------------------------------------------------------------------------------------")
+
 
 }
